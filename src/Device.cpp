dac67675777547a6d56267c21a1e2b3b8d566964
tymy2 <gabribouzas@gmail.com>
2024-06-05 11:25:35 +0200

    moved setup  to device

--- src/Device.cpp
+++ src/Device.cpp
@@ -6,6 +6,13 @@ void Device::init(){
 	this->cpu.device = this;
 	setup_instructions(this);
 	setup_cb_instructions(this);
+	setup_starting_state(this);
+}
+
+void Device::tick(){
+	if(this->debug_enabled) debug_print(this); 
+	this->cpu.decode_and_execute();
+	this->ppu.tick(this->cpu.index_cycles);
 }
 
 // TODO change implementation, it is really bad to output to console, speed goes really down
@@ -36,8 +43,42 @@ void debug_print(Device * device){
 	);
 }
 
-void Device::tick(){
-	if(this->debug_enabled) debug_print(this); 
-	this->cpu.decode_and_execute();
-	this->ppu.tick(this->cpu.index_cycles);
+void setup_starting_state(Device * device){
+	device->cpu.rr[0x00] = 0x01B0;
+	device->cpu.rr[0x01] = 0x0013;
+	device->cpu.rr[0x02] = 0x00D8;
+	device->cpu.rr[0x03] = 0x014D;
+	device->cpu.pc = 0x100;
+	device->cpu.sp = 0xFFFE;
+  	device->mmu.memory[0xFF05] = 0x00;
+  	device->mmu.memory[0xFF06] = 0x00;
+  	device->mmu.memory[0xFF07] = 0x00;
+	device->mmu.memory[0xFF10] = 0x80;
+ 	device->mmu.memory[0xFF11] = 0xBF;
+    device->mmu.memory[0xFF12] = 0xF3;
+	device->mmu.memory[0xFF14] = 0xBF;
+    device->mmu.memory[0xFF16] = 0x3F;
+    device->mmu.memory[0xFF17] = 0x00;
+    device->mmu.memory[0xFF19] = 0xBF;
+    device->mmu.memory[0xFF1A] = 0x7F;
+    device->mmu.memory[0xFF1B] = 0xFF;
+    device->mmu.memory[0xFF1C] = 0x9F;
+    device->mmu.memory[0xFF1E] = 0xBF;
+    device->mmu.memory[0xFF20] = 0xFF;
+    device->mmu.memory[0xFF21] = 0x00;
+    device->mmu.memory[0xFF22] = 0x00;
+    device->mmu.memory[0xFF23] = 0xBF;
+    device->mmu.memory[0xFF24] = 0x77;
+    device->mmu.memory[0xFF25] = 0xF3;
+    device->mmu.memory[0xFF26] = 0xF1;
+	device->mmu.memory[0xFF40] = 0x91;
+    device->mmu.memory[0xFF42] = 0x00;
+    device->mmu.memory[0xFF43] = 0x00;
+    device->mmu.memory[0xFF45] = 0x00;
+    device->mmu.memory[0xFF47] = 0xFC;
+    device->mmu.memory[0xFF48] = 0xFF;
+    device->mmu.memory[0xFF49] = 0xFF;
+    device->mmu.memory[0xFF4A] = 0x00;
+    device->mmu.memory[0xFF4B] = 0x00;
+    device->mmu.memory[0xFFFF] = 0x00;
 }
--- src/Goatboy.cpp
+++ src/Goatboy.cpp
@@ -15,46 +15,6 @@ public:
 		this->device.ppu.memory = this->device.mmu.memory; // setup memory pointer for ppu
 		this->setup_starting_state();
 	}
-
-	void setup_starting_state(){
-		this->device.cpu.rr[0x00] = 0x01B0;
-		this->device.cpu.rr[0x01] = 0x0013;
-		this->device.cpu.rr[0x02] = 0x00D8;
-		this->device.cpu.rr[0x03] = 0x014D;
-		this->device.cpu.pc = 0x100;
-		this->device.cpu.sp = 0xFFFE;
-	  	this->device.mmu.memory[0xFF05] = 0x00;
-	  	this->device.mmu.memory[0xFF06] = 0x00;
-	  	this->device.mmu.memory[0xFF07] = 0x00;
-		this->device.mmu.memory[0xFF10] = 0x80;
-	 	this->device.mmu.memory[0xFF11] = 0xBF;
-	    this->device.mmu.memory[0xFF12] = 0xF3;
-		this->device.mmu.memory[0xFF14] = 0xBF;
-	    this->device.mmu.memory[0xFF16] = 0x3F;
-	    this->device.mmu.memory[0xFF17] = 0x00;
-	    this->device.mmu.memory[0xFF19] = 0xBF;
-	    this->device.mmu.memory[0xFF1A] = 0x7F;
-	    this->device.mmu.memory[0xFF1B] = 0xFF;
-	    this->device.mmu.memory[0xFF1C] = 0x9F;
-	    this->device.mmu.memory[0xFF1E] = 0xBF;
-	    this->device.mmu.memory[0xFF20] = 0xFF;
-	    this->device.mmu.memory[0xFF21] = 0x00;
-	    this->device.mmu.memory[0xFF22] = 0x00;
-	    this->device.mmu.memory[0xFF23] = 0xBF;
-	    this->device.mmu.memory[0xFF24] = 0x77;
-	    this->device.mmu.memory[0xFF25] = 0xF3;
-	    this->device.mmu.memory[0xFF26] = 0xF1;
-		this->device.mmu.memory[0xFF40] = 0x91;
-	    this->device.mmu.memory[0xFF42] = 0x00;
-	    this->device.mmu.memory[0xFF43] = 0x00;
-	    this->device.mmu.memory[0xFF45] = 0x00;
-	    this->device.mmu.memory[0xFF47] = 0xFC;
-	    this->device.mmu.memory[0xFF48] = 0xFF;
-	    this->device.mmu.memory[0xFF49] = 0xFF;
-	    this->device.mmu.memory[0xFF4A] = 0x00;
-	    this->device.mmu.memory[0xFF4B] = 0x00;
-	    this->device.mmu.memory[0xFFFF] = 0x00;
-	}
 	
 	void run(){
 		while(this->keep_running){
